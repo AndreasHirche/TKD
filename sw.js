3c268fce1b116706ccfe8c0f2e3d6510261b0456
Andreas Hirche <andreas.hirche@sap.com>
2026-05-16 12:02:23 +0200

    feat(TKD): PWA installable + smaller announce text (2/3 size)

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'tkd-v1';
+
+self.addEventListener('install', e => { self.skipWaiting(); });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
